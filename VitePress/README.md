8dca65506cb3a62fc0cacba5d74909656b7bf768
高江田 <857669126@qq.com>
2022-07-26 17:30:19 +0800

    feat(VitePress): learn2

---
+++ VitePress/README.md
@@ -0,0 +1,7 @@
+## 项目结构目录
+.
+├─ docs
+│  ├─ .vitepress
+│  │  └─ config.ts
+│  └─ index.md // 入口主页
+└─ package.json
--- VitePress/docs/.vitepress/config.ts
+++ VitePress/docs/.vitepress/config.ts
@@ -1,15 +1,193 @@
 import { defineConfig } from 'vitepress'
+import renderPermaLink from './render-perma-link'
+import MarkDownItCustomAnchor from './markdown-it-custom-anchor'
 export default defineConfig({
-  base: '/FELearnNodes/',
-  title: '前端学习笔记',
+  title: '测试',
   description: '在学习前端过程中总结的笔记、自己对知识的沉淀和思考',
   lang: 'zh-CN',
   themeConfig: {
+    // logo: '/logo.svg',
+    nav: [
+      { text: '指引', link: '/guide/', activeMatch: '/guide/' },
+      { text: '配置', link: '/config/', activeMatch: '/config/' },
+      { text: '插件', link: '/plugins/', activeMatch: '/plugins/' },
+      {
+        text: '相关链接',
+        items: [
+          { text: 'Team', link: '/team' },
+          {
+            text: 'Twitter',
+            link: 'https://twitter.com/vite_js'
+          },
+          {
+            text: 'Discord Chat',
+            link: 'https://chat.vitejs.dev'
+          },
+          {
+            text: 'Awesome Vite',
+            link: 'https://github.com/vitejs/awesome-vite'
+          },
+          {
+            text: 'Dev.to 社区',
+            link: 'https://dev.to/t/vite'
+          },
+          {
+            text: 'Rollup 插件兼容',
+            link: 'https://vite-rollup-plugins.patak.dev/'
+          },
+          {
+            text: '更新日志',
+            link: 'https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md'
+          }
+        ]
+      },
+      {
+        text: 'Version',
+        items: [
+          {
+            text: 'Vite v2 文档',
+            link: 'https://v2.vitejs.dev'
+          }
+        ]
+      }
+    ],
+    socialLinks: [
+      { icon: 'twitter', link: '#' },
+      { icon: 'discord', link: '#' },
+      { icon: 'github', link: '#' }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          text: '指引',
+          items: [
+            {
+              text: '为什么选 Vite',
+              link: '/guide/why'
+            },
+            {
+              text: '开始',
+              link: '/guide/'
+            },
+            {
+              text: '功能',
+              link: '/guide/features'
+            },
+            {
+              text: '使用插件',
+              link: '/guide/using-plugins'
+            },
+            {
+              text: '依赖预构建',
+              link: '/guide/dep-pre-bundling'
+            },
+            {
+              text: '静态资源处理',
+              link: '/guide/assets'
+            },
+            {
+              text: '构建生产版本',
+              link: '/guide/build'
+            },
+            {
+              text: '部署静态站点',
+              link: '/guide/static-deploy'
+            },
+            {
+              text: '环境变量与模式',
+              link: '/guide/env-and-mode'
+            },
+            {
+              text: '服务端渲染（SSR）',
+              link: '/guide/ssr'
+            },
+            {
+              text: '后端集成',
+              link: '/guide/backend-integration'
+            },
+            {
+              text: '比较',
+              link: '/guide/comparisons'
+            },
+            {
+              text: '从 v2 迁移',
+              link: '/guide/migration'
+            }
+          ]
+        },
+        {
+          text: 'API',
+          items: [
+            {
+              text: '插件 API',
+              link: '/guide/api-plugin'
+            },
+            {
+              text: 'HMR API',
+              link: '/guide/api-hmr'
+            },
+            {
+              text: 'JavaScript API',
+              link: '/guide/api-javascript'
+            },
+            {
+              text: '配置参考',
+              link: '/config/'
+            }
+          ]
+        }
+      ],
+      '/config/': [
+        {
+          text: '配置',
+          items: [
+            {
+              text: '配置 Vite',
+              link: '/config/'
+            },
+            {
+              text: '共享选项',
+              link: '/config/shared-options'
+            },
+            {
+              text: '服务器选项',
+              link: '/config/server-options'
+            },
+            {
+              text: '构建选项',
+              link: '/config/build-options'
+            },
+            {
+              text: '预览选项',
+              link: '/config/preview-options'
+            },
+            {
+              text: '依赖优化选项',
+              link: '/config/dep-optimization-options'
+            },
+            {
+              text: 'SSR 选项',
+              link: '/config/ssr-options'
+            },
+            {
+              text: 'Worker 选项',
+              link: '/config/worker-options'
+            }
+          ]
+        }
+      ]
+    },
     footer: {
       message: '根据 MIT 许可证发布。',
       copyright: 'Copyright © 2022-present vaeian & FELearnNodes Contributors'
+    }
+  },
+  markdown: {
+    anchor: {
+      permalink: renderPermaLink,
     },
-    nav: [
-    ],
+    config: (md) => {
+      md.use(MarkDownItCustomAnchor)
+    }
   }
 })
