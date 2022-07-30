af9e1510529b86e5d6a34b9a4cdf1d978b3e2ae8
vaesonshu <w857669126@gmail.com>
2022-07-30 23:37:25 +0800

    feat(nuxt3): learn nuxt3 base

---
+++ Nuxt3/nuxt3-demo/pages/home.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { data } = await useAsyncData('ssr', () =>
+  $fetch('https://m.maoyan.com/ajax/movieOnInfoList')
+)
+const listData = data.value.movieList
+</script>
+
+<template>
+  <div v-for="(item, index) in listData" :key="index">
+    <h1>{{ item.nm }}</h1>
+    <h2>{{ item.star }}</h2>
+    <img :src="item.img" style="width: 300px; height: 400px" />
+  </div>
+</template>
